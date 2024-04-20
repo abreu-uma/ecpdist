025ad2a1b838bba823b9d482d92527904452e907
abreu-uma <abreu@staff.uma.pt>
2024-04-20 17:06:28 +0100

    add survival funtion code

---
+++ R/hecp.R
@@ -0,0 +1,48 @@
+#### Hazard function ####
+
+#' Hazard function
+#'
+#' @description
+#' Compute the extended Chen-Poisson (ecp) hazard function.
+#'
+#' @param x vector of quantiles.
+#'
+#' @param lambda,gamma  parameter values > 0.
+#'
+#' @param phi parameter value > 0 or < 0.
+#'
+#' @param log logical value
+#'
+#' @return Numeric value of the hazard function.
+#'
+#' @return If log = TRUE, numeric value of the logarithm of the function.
+#'
+#' @examples
+#' hecp(2, 1, 1, 1, log = FALSE) # hazard function
+#'
+#' @export
+hecp <- function(x, lambda, gamma, phi, log = FALSE) {
+  # Check if arguments are numeric
+  if (!all(sapply(list(x, lambda, gamma, phi), is.numeric))) {
+    stop("non-numeric argument")
+  }
+
+  # Check for invalid arguments
+  if (any(x < 0) || min(lambda <= 0) || min(gamma <= 0) ||
+        min(phi == 0)) {
+    stop("Invalid arguments")
+  }
+
+  # Calculate hazard function
+
+  hf <- (lambda * gamma * phi * x^(gamma - 1) *
+           exp(x^gamma + lambda * (1 - exp(x^gamma)))) /
+    (exp(phi * exp(lambda * (1 - exp(x^gamma)))) - 1)
+
+  # Convert hazard function to log scale if log is TRUE
+  if (log)
+    hf <- log(hf)
+
+  return(hf)
+
+}
---
+++ R/secp.R
@@ -0,0 +1,59 @@
+#### Survival function ####
+
+#' Survival function
+#'
+#' @description
+#' Compute the extended Chen-Poisson (ecp) survival function.
+#'
+#' @param q vector of quantiles.
+#'
+#' @param lambda,gamma  parameter values > 0.
+#'
+#' @param phi parameter value > 0 or < 0.
+#'
+#' @param lower_tail similar to lower.tail
+#'
+#' @param cum_haz logical value
+#'
+#' @return Numeric value of the survival function.
+#'
+#' @return If lower_tail = TRUE, numeric value of the distribution function.
+#'
+#' @return If cum_haz = TRUE, numeric value of the cumulative hazard function.
+#'
+#' @examples
+#' secp(2, 1, 1, 1, lower_tail = FALSE, cum_haz = FALSE) # survival function
+#' secp(2, 1, 1, 1, lower_tail = TRUE, cum_haz = FALSE) # distribution function
+#' secp(2, 1, 1, 1, lower_tail = FALSE, cum_haz = TRUE) # cumulative
+#' # hazard function
+#'
+#' @export
+secp <- function(q, lambda, gamma, phi, lower_tail = FALSE,
+                 cum_haz = FALSE) {
+  # Check if arguments are numeric
+  if (!all(sapply(list(q, lambda, gamma, phi), is.numeric))) {
+    stop("non-numeric argument")
+  }
+
+  # Check for invalid arguments
+  if (any(q < 0) || min(lambda <= 0) || min(gamma <= 0) ||
+        min(phi == 0)) {
+    stop("Invalid arguments")
+  }
+
+  # Calculate survival function
+  sf <- (1 - exp(-phi * exp(lambda * (1 - exp(q^gamma))))) /
+    (1 - exp(-phi))
+
+  # Adjust survival function if lower_tail is TRUE
+  if (lower_tail)
+    sf <- 1 - sf
+
+  # Calculate cumulative hazard function if cum_haz is TRUE
+
+  if (cum_haz)
+    sf <- -log(sf)
+
+  return(sf)
+
+}
