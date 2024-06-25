b5bc08a13cb79ebb62d294d1652ebf97e7c3c782
abreu-uma <abreu@staff.uma.pt>
2024-06-25 17:10:17 +0100

    Add some text.

--- R/plot_ecp.R
+++ R/plot_ecp.R
@@ -3,10 +3,11 @@
 #' Plot function
 #'
 #' @description
-#' Plots the density, cumulative distribution, hazard and survival functions of the
-#' extended Chen-Poisson (ecp) distribution.
+#' Plots the density, cumulative distribution, hazard, survival and quantile
+#' functions of the extended Chen-Poisson (ecp) distribution.
 #'
-#' @param data_type either a x vector of data values or an expression.
+#' @param data_type specifies whether the input is a x vector of data values or
+#' an expression.
 #'
 #' @param from lower x axis limit.
 #'
@@ -24,7 +25,7 @@
 #'
 #' @param log logical value.
 #'
-#' @param func_type type of function to be plotted.
+#' @param func_type specifies the type of function to be plotted.
 #'
 #' @param title title of the graphic.
 #'
@@ -37,112 +38,79 @@
 #' @return graphic of the chosen ecp function.
 #'
 #' @examples
-#' ecp_plot(data_type = "expression", func_type = "hazard",
-#' title = "Hazard Function (Expression)") # hazard function graphic
 #'
+#' ecp_plot(data_type = "expression", from = 0, to = 1,
+#' func_type = "cumulative distribution",
+#' title = "Cumulative Distribution Function (Expression)")
+#' #  Example of plotting cumulative distribution using an expression
 #' @export
 #'
+
+# Define the function for plotting
+
 ecp_plot <- function(data_type, from = NULL, to = NULL, xlim = NULL,
                      ylim = NULL, x = NULL, lambda = 6, gamma = 0.3, phi = 30,
                      log = FALSE, func_type, title, col = "black", lty = 1) {
 
   # Set default plotting range if not provided
-
   if (is.null(from)) from <- 0
-  if (is.null(to)) to <- 3
+  if (is.null(to)) to <- 1
   if (is.null(xlim)) xlim <- c(from, to)
 
-  # Determine the expression based on func_type
-
-  expr <- switch(func_type,
-                 "density" = "decp(x, lambda, gamma, phi, log)",
-                 "hazard" = "hecp(x, lambda, gamma, phi, log)",
-                 "survival" = "secp(x, lambda, gamma, phi, lower_tail = FALSE,
-                 cum_haz = FALSE)",
-                 "cumulative distribution" = "pecp(x, lambda, gamma, phi,
-                 lower_tail = TRUE, log_p = FALSE)",
-
-                 stop("Invalid function type. Use 'density' or 'hazard'."))
-
-  # Plotting based on data type and func_type
+  # Define the expressions based on func_type
+  func <- switch(func_type,
+                 "density" = function(x) decp(x, lambda, gamma, phi, log),
+                 "hazard" = function(x) hecp(x, lambda, gamma, phi, log),
+                 "survival" = function(x) {
+                                           secp(x, lambda, gamma, phi,
+                                                lower_tail = FALSE,
+                                                cum_haz = FALSE)},
+                 "cumulative distribution" = function(x) {
+                                                          pecp(x, lambda, gamma,
+                                                               phi,
+                                                               lower_tail =
+                                                                 TRUE,
+                                                               log_p = FALSE)},
+                 "quantile" = function(x) {
+                                           qecp(x, lambda, gamma, phi,
+                                                lower_tail = TRUE,
+                                                log_p = FALSE)},
+                 stop("Invalid function type. Use 'density', 'hazard',
+                      'survival', 'cumulative distribution' or 'quantile'."))
 
   if (data_type == "expression") {
+    # Plot based on expression
+    ylab <- switch(func_type,
+                   "density" = "Density",
+                   "hazard" = "Hazard",
+                   "survival" = "Survival",
+                   "cumulative distribution" = "Cumulative Distribution",
+                   "quantile" = "Quantile")
 
-    # Parse and evaluate the expression
-
-    expr_parsed <- parse(text = expr)
-
-    if (func_type == "density") {
-      curve(eval(expr_parsed, envir = list(x = x, lambda = lambda,
-                                           gamma = gamma, phi = phi,
-                                           log = log)),
-            from = from, to = to, xlim = xlim, ylim = ylim, col = col,
-            lty = lty, main = "", xlab = "x", ylab = "Density")
-
-    } else if (func_type == "hazard") {
-      curve(eval(expr_parsed, envir = list(x = x, lambda = lambda,
-                                           gamma = gamma, phi = phi,
-                                           log = log)),
-            from = from, to = to, xlim = xlim, ylim = ylim, col = col,
-            lty = lty, main = "", xlab = "x", ylab = "Hazard")
-    } else if (func_type == "survival") {
-      curve(eval(expr_parsed, envir = list(x = x, lambda = lambda,
-                                           gamma = gamma, phi = phi,
-                                           lower_tail = FALSE,
-                                           cum_haz = FALSE)),
-            from = from, to = to, xlim = xlim, ylim = ylim, col = col,
-            lty = lty, main = "", xlab = "x", ylab = "Survival")
-    } else if (func_type == "cumulative distribution") {
-      curve(eval(expr_parsed, envir = list(x = x, lambda = lambda,
-                                           gamma = gamma, phi = phi,
-                                           lower_tail = TRUE,
-                                           log_p = FALSE)),
-            from = from, to = to, xlim = xlim, ylim = ylim, col = col,
-            lty = lty, main = "", xlab = "x",
-            ylab = "Cumulative distribution")
-
-    }
+    curve(expr = func, from = from, to = to, xlim = xlim, ylim = ylim,
+          col = col, lty = lty, main = title, xlab = "x", ylab = ylab)
 
   } else if (data_type == "data") {
     if (is.null(x)) {
       stop("Data points must be provided for data type 'data'.")
     }
-
-    if (func_type == "density") {
-      y_values <- decp(x, lambda, gamma, phi, log = log)
-      plot(x, y_values, type = "l", xlim = xlim, ylim = ylim, col = col,
-           lty = lty, main = "", xlab = "x", ylab = "Density")
-
-    } else if (func_type == "hazard") {
-      y_values <- hecp(x, lambda, gamma, phi, log = log)
-      plot(x, y_values, type = "l", xlim = xlim, ylim = ylim, col = col,
-           lty = lty, main = "", xlab = "x", ylab = "Hazard")
-    } else if (func_type == "survival") {
-      y_values <- secp(x, lambda, gamma, phi, lower_tail = FALSE,
-                       cum_haz = FALSE)
-      plot(x, y_values, type = "l", xlim = xlim, ylim = ylim, col = col,
-           lty = lty, main = "", xlab = "x", ylab = "Survival")
-    } else if (func_type == "cumulative distribution") {
-      y_values <- pecp(x, lambda, gamma, phi, lower_tail = TRUE, log_p = FALSE)
-      plot(x, y_values, type = "l", xlim = xlim, ylim = ylim, col = col,
-           lty = lty, main = "", xlab = "x", ylab = "Cumulative distribution")
-    }
+    y_values <- switch(func_type,
+                       "density" = decp(x, lambda, gamma, phi, log),
+                       "hazard" = hecp(x, lambda, gamma, phi, log),
+                       "survival" = secp(x, lambda, gamma, phi,
+                                         lower_tail = FALSE, cum_haz = FALSE),
+                       "cumulative distribution" = pecp(x, lambda, gamma, phi,
+                                                        lower_tail = TRUE,
+                                                        log_p = FALSE),
+                       "quantile" = qecp(x, lambda, gamma, phi,
+                                         lower_tail = TRUE, log_p = FALSE),
+                       stop("Invalid function type. Use 'density', 'hazard',
+                            'survival', 'cumulative distribution' or
+                            'quantile'."))
+    plot(x, y_values, type = "l", xlim = xlim, ylim = ylim, col = col,
+         lty = lty, main = title, xlab = "x", ylab = func_type)
 
   } else {
     stop("Invalid data type. Use 'expression' or 'data'.")
   }
-
-  # Set the title after plotting
-
-  title(main = title, adj = 0.5)
 }
-
-# Plotting density using an expression
-
-ecp_plot(
-  data_type = "expression",
-  from = 0,
-  to = 3,
-  func_type = "density",
-  title = "Density Function (Expression)"
-)
